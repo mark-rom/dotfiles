1a65ea1c4907eb56ca341ec174150ccd9a481bd1
Pavel Sergeev <pavel.al.sergeev@gmail.com>
2026-03-28 23:58:55 +0100

    added commands to setup macbook

--- setup.sh
+++ setup.sh
@@ -3,7 +3,22 @@
 # brew installation
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# nerd font installation
-brew install --cask font-ubuntu-nerd-font
-brew install git pyenv starship
+brew install --cask 1password-cli \
+    finicky \
+    font-ubuntu-nerd-font
+
 # required packages installation
+brew install \
+    git \
+    pure \
+    zsh-completions \
+    tree \
+    pyenv \
+    allure \
+    node \
+    yarn
+
+# create symlink to .zshrc
+ln -sf "$(pwd)"/.zshrc ~/.zshrc
+ln -sf "$(pwd)"/.vimrc ~/.vimrc
+
